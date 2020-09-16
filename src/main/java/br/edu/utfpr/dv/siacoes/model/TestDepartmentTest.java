b3f644b7498e968d746ec67ae3ceb64946fbee77
Luis Gustavo Vital <luisgustavo2829@gmail.com>
2020-09-15 21:21:55 -0300

    JUnit - Test Department

---
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/TestDepartmentTest.java
@@ -0,0 +1,15 @@
+package br.edu.utfpr.dv.siacoes.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestDepartmentTest {
+
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFullName() {
+    }
+}
